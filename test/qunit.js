291d0aef3478fce175755c6bb3c78c9a976b203b
eauthentik <ernosalo14@gmail.com>
2018-12-08 19:24:11 +0200

    QUnit.js test code for Django and jQuery
    Still learning QUnit

--- test/qunit.js
+++ test/qunit.js
@@ -1,3 +1,22 @@
-QUnit.test( "hello test", function( assert ) {
-    assert.ok( 1 == "1", "Passed!" );
-  });
+QUnit.module('magicTricks', {
+  beforeEach: function() {
+      var $ = django.jQuery;
+      $('#qunit-fixture').append('<button class="button"></button>');
+  }
+});
+
+QUnit.test('removeOnClick removes button on click', function(assert) {
+  var $ = django.jQuery;
+  removeOnClick('.button');
+  assert.equal($('.button').length, 1);
+  $('.button').click();
+  assert.equal($('.button').length, 0);
+});
+
+QUnit.test('copyOnClick adds button on click', function(assert) {
+  var $ = django.jQuery;
+  copyOnClick('.button');
+  assert.equal($('.button').length, 1);
+  $('.button').click();
+  assert.equal($('.button').length, 2);
+});
